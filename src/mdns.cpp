353f72dbaea4c656d948381bacac239c7d3324ac
Tal Aviram <me@talaviram.com>
2024-11-05 15:16:13 +0200

    executeQuery - warn if user tries to have more queries than supported.

--- src/mdns.cpp
+++ src/mdns.cpp
@@ -733,6 +733,12 @@ void mDNS::executeQuery(ServiceQueries serviceQueries) {
   int query_id[32];
   int num_sockets = openClientSockets(sockets, sizeof(sockets) / sizeof(sockets[0]), 0);
 
+  if (serviceQueries.size() > 32) {
+    const auto msg = "Max supported service queries is 32.";
+    MDNS_LOG << msg << "\n";
+    throw std::runtime_error(msg);
+  }
+
   if (num_sockets <= 0) {
     const auto msg = "Failed to open any client sockets";
     MDNS_LOG << msg << "\n";
