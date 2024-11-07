7c99563292c0322e3f586136ce58bd4bd27fdccb
Tal Aviram <me@talaviram.com>
2024-11-07 13:35:28 +0200

    Add/refactored executeQuery to support lambda.

--- include/mdns_cpp/mdns.hpp
+++ include/mdns_cpp/mdns.hpp
@@ -68,6 +68,7 @@ class mDNS {
 
   using ServiceQueries = std::vector<std::pair<std::string, RecordType>>;
   std::vector<Record> executeQuery(ServiceQueries service);
+  void executeQuery(ServiceQueries service, std::function<void(Record)> onNewRecord);
   void executeDiscovery();
 
  private:
